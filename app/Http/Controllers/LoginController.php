b172f2896b7fe6bcb240cdc83ec3d17ccccdf6b9
ryumanmasanobu <22011376mr@tama.ac.jp>
2021-10-21 16:24:26 +0900

    複数ファイルのアップロード

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class MainController extends Controller
+class LoginController extends Controller
 {
     function show(){
         return view('login');
