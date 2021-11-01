a5edb0c9a9ceac4701a499a39c5b411aa1f1ef1a
ryumanmasanobu <22011376mr@tama.ac.jp>
2021-11-01 09:38:42 +0900

    react-js-paginationインストール 2

--- resources/js/components/UserContent.js
+++ resources/js/components/UserContent.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import ReactDOM from 'react-dom';
+import Pagination from "react-js-pagination";
 
 function UserContent() {
 
