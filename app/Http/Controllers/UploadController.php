1fb5d109329263613242a7e6720b52e9952c5f3b
ryumanmasanobu <22011376mr@tama.ac.jp>
2021-11-09 13:28:04 +0900

    ページングシステムの改善

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\userInfo;
 use App\Models\content;
+use App\Models\lotationVectorData;
 
 class UploadController extends Controller
 {
@@ -17,15 +18,7 @@ class UploadController extends Controller
         if (!is_null($objFile) and !is_null($mtlFile)) {
             date_default_timezone_set('Asia/Tokyo');
             $uniqName = uniqid();
-            $content = new content;
             $name = session('UserName','default');
-
-            if ($request->get('releaseSetting') == "public")
-            {
-                $content->addContent($name,$contentName,$uniqName,"public");
-            }else{
-                $content->addContent($name,$contentName,$uniqName,"private");
-            }
             $objName = $objFile->getClientOriginalName();
             $mtlName = $mtlFile->getClientOriginalName();
             $objFileType = pathinfo($objName, PATHINFO_EXTENSION);
@@ -35,6 +28,15 @@ class UploadController extends Controller
             $dir = 'public';
             $objFile->storeAs($dir, $objFileName, ['disk' => 'local']);
             $mtlFile->storeAs($dir, $mtlFileName, ['disk' => 'local']);
+            if ($request->get('releaseSetting') == "public")
+            {
+                $content->addContent($name,$contentName,$uniqName,"public");
+            }else{
+                $content->addContent($name,$contentName,$uniqName,"private");
+            }
+            $content = new content;
+            $lotationVectorData = new lotationVectorData;
+            $lotationVectorData->addLotationVector($name,$uniqName);
         }
 
         header("location: main");
