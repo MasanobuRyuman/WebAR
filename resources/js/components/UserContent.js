0216446f76313ef21b4d7f67e2c1fbf18d49b01b
ryumanmasanobu <22011376mr@tama.ac.jp>
2021-11-10 16:45:00 +0900

    userContentのバグの修正

--- resources/js/components/UserContent.js
+++ resources/js/components/UserContent.js
@@ -19,7 +19,7 @@ function UserContent() {
             setFirstUseEffect(false);
         }else{
             console.log(userContent);
-            pageButton();
+            buttonSet();
         }
     },[userContent]);
 
@@ -49,24 +49,37 @@ function UserContent() {
         setNowPage(pageNumber);
     }
 
-    function pageButton()
+    function buttonSet()
     {
         let prev = true;
         let outputPage = nowPage;
-        let list = [nowPage]
+        let list = [nowPage];
+        let pageCount = 0;
+        let addprevpage = userContent.data.last_page - outputPage;
+        console.log("pageCount");
         while (true){
+            console.log(outputPage);
             if (outputPage == 1){
+                prev = false;
+                outputPage = nowPage;
+            } else if(prev == true && pageCount == 5 + (5-addprevpage)){
                 prev = false;
                 outputPage = nowPage;
             }
 
             if (prev == true){
+                console.log("prevTrue");
                 outputPage -= 1;
+                pageCount += 1;
                 list.unshift(outputPage);
             }else if (outputPage == userContent.data.last_page){
                 break;
+            }else if (pageCount == 10){
+                break;
             }else{
                 outputPage += 1;
+                pageCount += 1;
+                console.log("add");
                 list.push(outputPage);
             }
         }
@@ -93,9 +106,9 @@ function UserContent() {
             {paging.map((data)=>(
                 <a key={data} onClick={() => move_page(data)}>{data}</a>
             ))}
-            <a onClick={add_current_page}　>次</a>
+            <a onClick={add_current_page}>次</a>
             <input name="saveName" type="hidden" value={saveName}></input>
-        
+
         </div>
     );
 }
