b457fc2086da49e292b7df1fed1244ef081e405d
ryumanmasanobu <22011376mr@tama.ac.jp>
2021-12-10 22:08:23 +0900

    button生成のオブジェクト化

--- resources/js/components/PublicSearchPage.js
+++ resources/js/components/PublicSearchPage.js
@@ -2,12 +2,22 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch , Link } from 'react-router-dom';
+import {PageButton} from './PageButton.js';
 
 export default function PublicSerchPage() {
     const [searchContent, setsearchContent] = useState([]);
+    const [nowPage,setNowPage] = useState(1);
+    let first = true;
     useEffect(()=>{
         getContent()
     },[])
+    useEffect(()=>{
+        if (first == true){
+            first = false;
+        }else{
+            setPageButton()
+        }
+    },[searchContent])
     const getContent = async ()=>{
         let searchBasedTagList = localStorage.getItem("searchBasedTag");
         searchBasedTagList = JSON.parse(searchBasedTagList);
@@ -18,9 +28,13 @@ export default function PublicSerchPage() {
             formData.append("searchBasedTagList[]",i.tagName);
         })
 
-        let request = await axios.post("./api/getSearchContentAPI",formData);
-        console.log(request);
+        let request = await axios.post(`./api/getSearchContentAPI?page=${nowPage}`,formData);
+        console.log(request.data.last_page);
         setsearchContent(request);
+    }
+    function setPageButton(){
+        let lastPage = searchContent.data.last_page;
+        console.log(PageButton(nowPage,lastPage));
     }
     return(
         <h1>検索画面</h1>
