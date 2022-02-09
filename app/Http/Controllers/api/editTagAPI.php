7b1759fc15baaa3283c3407e3494732d2f9054d7
ryumanmasanobu <22011376mr@tama.ac.jp>
2022-02-09 19:27:18 +0900

    タグ編集機能実装完了

--- app/Http/Controllers/api/editTagAPI.php
+++ app/Http/Controllers/api/editTagAPI.php
@@ -5,11 +5,27 @@ use Illuminate\Http\Request;
 use App\Models\contentInfo;
 use App\Models\contentPhoto;
 use App\Models\tagList;
+use App\Models\contentAndTag;
 class editTagAPI extends Controller
 {
     function edit(Request $request){
-        $selectedTagList = $request -> get('selectedTagList');
-
+        $selectedTagList = $_POST['selectedTagList'];
+        $saveName = $_POST["saveName"];
         logger("選択されたtagu",["選択されたタグ"=>$selectedTagList]);
+        $tagList = new tagList;
+        $tagID = $tagList->gettagId($selectedTagList);
+        $tagIDList = array();
+        foreach($tagID as $temp){
+            foreach($temp as $temp2){
+                foreach($temp2 as $temp3){
+                    $tagIDList[]=$temp3;
+                }
+            }
+        }
+        logger("選択されたタグID",["選択されたタグID"=>$tagIDList]);
+        $contentAndTag = new contentAndTag;
+        $contentAndTag->allDeleteTagDataBySaveName($saveName);
+        $contentAndTag->addConditionOfTagId($saveName,$tagIDList);
+
     }
 }
