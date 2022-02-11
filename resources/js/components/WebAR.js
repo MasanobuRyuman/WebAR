1e7c5927d4edfad1a9380daa646589713e809786
ryumanmasanobu <22011376mr@tama.ac.jp>
2022-02-11 14:11:27 +0900

    標準のARマーカーに認証完了

--- resources/js/components/WebAR.js
+++ resources/js/components/WebAR.js
@@ -88,7 +88,7 @@ function WebAR() {
                     <a-asset-item id="cow" src={obj}></a-asset-item>
                     <a-asset-item id="mtl" src={mtl}></a-asset-item>
                 </a-assets>
-                <a-marker preset="hiro">
+                <a-marker type='pattern' url="storage/ar_marker/pattern-marker.patt" >
                     <a-obj-model src="#cow" mtl="#mtl" scale=".5 .5 .5" rotation={vectorData}></a-obj-model>
                 </a-marker>
             </a-scene>
